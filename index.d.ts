9b3cbead28876df2059ef9387babd670ad55fcc1
Israel Fonseca <israel@mtn.com.br>
2022-10-11 23:46:50 -0300

    definition file added

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,28 @@
-declare namespace Mtn {
-	interface Options {	
-		
-	}
-}
-
-declare const Mtn: {	
-	default: typeof Mtn;
+declare const Mtn: {
+    utils: {
+        createNamespace: (namespace: any) => any;
+        isNullOrUndefined: (o: any) => any;
+    };
+    ns: (namespace: any) => void;
+    getUrl: () => any;
+    isNullOrUndefined: (o: any) => boolean;
+    isEmpty: (o: any) => boolean;
+    id: (el: any, matchDate: any, sep: any) => any;
+    getType: (obj: any) => any;
+    isFunction: (obj: any) => any;
+    reset: (obj: any) => void;
+    trim: (text: any) => string;
+    removeAt: (array: any, pos: any) => any;
+    lambda: (query: any) => Function;
+    whereMtn: (array: any, query: any) => any[];
+    eachMtn: (array: any, query: any, fnCall: any) => any[];
+    eachInverseMtn: (array: any, query: any, fnCall: any) => any[];
+    firstMtn: (array: any, query: any) => any;
+    lastMtn: (array: any, query: any) => any;
+    anyMtn: (array: any, query: any) => boolean;
+    clearAllTimeouts: () => void;
+    clone: (baseObj: any, o: any) => any;
 };
+
 
 export = Mtn;
